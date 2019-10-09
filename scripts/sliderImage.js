5cf4c5fe1a7cd1099067d5a80597050c9731efcb
Ajay Ramachandran <dev@ajay.app>
2019-10-08 20:02:46 -0400

    Added proof of concept sliding image.

---
+++ scripts/sliderImage.js
@@ -0,0 +1,48 @@
+
+var mouseX = -1;
+
+function init() {
+    let container = document.getElementById("slider-container")
+
+    //update mouse position
+    container.addEventListener("mousemove", function(ev) {
+        mouseX = ev.offsetX;
+    });
+
+    // runs the animations of the slider
+    setInterval(updateSlider, 0)
+}
+
+//used to calculate the delta time for animation smoothness
+var lastTime = -1;
+
+function updateSlider() {
+    if (mouseX == -1) return;
+
+    let deltaTime = (Date.now() - lastTime) / 1000;
+    if (deltaTime > 0.4) {
+        deltaTime = 1/60;
+    }
+
+    let image = document.getElementById("before-image");
+
+    let targetWidth = mouseX / document.body.clientWidth * 100;
+
+    //this is in the form of a percentage in a string
+    let previousWidthFormatted = image.style.width;
+    let previousWidth = previousWidthFormatted.slice(0, previousWidthFormatted.length - 1)
+
+    //interpolation is used to make it look smoother
+    let lerpFactor = 10 * deltaTime;
+
+    let distanceToTarget = (targetWidth - parseFloat(previousWidth));
+
+    let newWidth = parseFloat(previousWidth) + distanceToTarget * lerpFactor;
+
+    image.style.width = newWidth + "%";
+
+    lastTime = Date.now();
+}
+
+//run init on load
+document.addEventListener("DOMContentLoaded", init);
