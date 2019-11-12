ffc9a4ad160ddd10a13f2021e73aa795dfc55172
Ajay Ramachandran <dev@ajay.app>
2019-11-12 18:49:21 -0500

    Added basic color selector functionality.
    Also increased saturation on the product images.

--- scripts/products-selector.js
+++ scripts/products-selector.js
@@ -1,5 +1,9 @@
 var buttons = [];
 
+var mouseY = -1;
+
+var overlay;
+
 function init() {
     // Add listeners
     buttons = document.querySelectorAll(".product-preview-button");
@@ -8,6 +12,21 @@ function init() {
         let currentButton = buttons[i];
         currentButton.addEventListener("click", () => void openProduct(currentButton));
     }
+
+    let colorSelector = document.getElementById("product-color-selector");
+
+    //update mouse position
+    document.body.addEventListener("mousemove", function(ev) {
+        //ev.buttons == 1
+        if (ev.buttons == 1) {
+            mouseY = ev.clientY;
+        }
+    });
+
+    overlay = document.getElementById("overlay-product-image");
+
+    // runs the animations of the slider
+    setInterval(() => updateColorSlider(colorSelector), 0)
 }
 
 function openProduct(button) {
@@ -35,6 +54,51 @@ function openProduct(button) {
             description.classList.add("hidden");
         }
     }
+}
+
+//used to calculate the delta time for animation smoothness
+var lastTime = -1;
+
+function updateColorSlider(colorSelector) {
+    if (mouseY == -1) return;
+
+    let deltaTime = (Date.now() - lastTime) / 1000;
+    if (deltaTime > 0.4) {
+        deltaTime = 1/60;
+    }
+
+    let margin = 125;
+
+    let borderObject = document.getElementById("color-selector-border");
+
+    let localY = mouseY - margin;
+
+    let targetHeight = localY / colorSelector.style.height * 100;
+    //don't go over 100%
+    if (targetHeight > 100) {
+        targetHeight = 100;
+    }
+
+
+    //this is in the form of a percentage in a string
+    let previousWidthFormatted = borderObject.style.marginTop;
+    let previousWidth = previousWidthFormatted.slice(0, previousWidthFormatted.length - 2)
+    if (isNaN(parseFloat(previousWidth))) {
+        previousWidth = 0;
+    }
+
+    //interpolation is used to make it look smoother
+    let lerpFactor = 10 * deltaTime;
+
+    let distanceToTarget = (localY - parseFloat(previousWidth));
+
+    let newWidth = parseFloat(previousWidth) + distanceToTarget * lerpFactor;
+
+    borderObject.style.marginTop = newWidth + "px";
+
+    overlay.style.filter = "hue-rotate(" + (newWidth / colorSelector.clientHeight * 360) + "deg)"
+
+    lastTime = Date.now();
 }
 
 //run init on load
